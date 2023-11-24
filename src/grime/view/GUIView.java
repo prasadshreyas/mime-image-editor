f92d346eee42db8d2164ca566da2578e6bd90ff2
DevashishMahurkar <mahurkar.d@northeastern.edu>
2023-11-24 06:23:41 +0530

    First Push

--- src/grime/view/GUIView.java
+++ src/grime/view/GUIView.java
@@ -1,8 +1,39 @@
 package grime.view;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * This class represents the Graphical User Interface view for the program.
@@ -11,15 +42,29 @@ public class GUIView extends JFrame implements View {
   private static final long serialVersionUID = 1L;
   private JPanel buttonPanel;
 
+  private JPanel mainPanel;
+
+  private JPanel imagePanel;
+
+  private JScrollPane mainScrollPane;
+  private JLabel fileOpenDisplay;
+  private JLabel fileSaveDisplay;
+
+  private JLabel imageLabel;
+
+ private JScrollPane imageScrollPane;
+
+
   /**
    * Constructs a GUIView object and initializes it to display the GUI.
    */
   public GUIView() {
     super();
+
     initializeWindow();
 
 
-    initializeButtonPanel();
+   initializeButtonPanel();
 
 
     setVisible(true);
@@ -38,15 +83,34 @@ public class GUIView extends JFrame implements View {
       int returnValue = fileChooser.showOpenDialog(null);
       if (returnValue == JFileChooser.APPROVE_OPTION) {
         System.out.println(fileChooser.getSelectedFile().getPath());
+        updateImage(fileChooser.getSelectedFile().getPath());
+        //
       }
     });
     buttonPanel.add(loadButton);
+
+    //Save
+    JButton saveButton = new JButton("Save");
+    saveButton.setActionCommand("save");
+    saveButton.addActionListener(e -> {
+      JFileChooser fileChooser = new JFileChooser();
+      int returnValue = fileChooser.showOpenDialog(null);
+      if (returnValue == JFileChooser.APPROVE_OPTION) {
+        System.out.println(fileChooser.getSelectedFile().getPath());
+        //updateImage(fileChooser.getSelectedFile().getPath());
+        File f = fileChooser.getSelectedFile();
+        //CLI to save image
+      }
+    });
+    buttonPanel.add(saveButton);
+
+
+
 
     // quit button
     commandButton = new JButton("Quit");
     commandButton.setActionCommand("quit");
     commandButton.addActionListener(e -> System.exit(0));
-
     buttonPanel.add(commandButton);
 
     //
@@ -60,6 +124,31 @@ public class GUIView extends JFrame implements View {
     setSize(800, 600);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLayout(new BorderLayout());
+
+    mainPanel = new JPanel();
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+    mainScrollPane = new JScrollPane(mainPanel);
+    add(mainScrollPane);
+
+    JPanel imagePanel = new JPanel();
+    imagePanel.setBorder(BorderFactory.createTitledBorder("Showing an image"));
+    imagePanel.setLayout(new GridLayout(1, 0, 10, 10));
+    mainPanel.add(imagePanel);
+
+    String images = "res/Penguins.jpg";
+    imageLabel = new JLabel();
+    imageScrollPane = new JScrollPane(imageLabel);
+    imageLabel.setIcon(new ImageIcon(images));
+    imageScrollPane.setPreferredSize(new Dimension(100, 400));
+    imagePanel.add(imageScrollPane);
+
+
+    //dialog boxes
+    JPanel dialogBoxesPanel = new JPanel();
+    dialogBoxesPanel.setBorder(BorderFactory.createTitledBorder("Dialog boxes"));
+    dialogBoxesPanel.setLayout(new BoxLayout(dialogBoxesPanel, BoxLayout.PAGE_AXIS));
+    mainPanel.add(dialogBoxesPanel);
+
   }
 
 
@@ -68,4 +157,11 @@ public class GUIView extends JFrame implements View {
     JOptionPane.showMessageDialog(this, message);
 
   }
+
+  private void updateImage(String Filename)
+  {
+    imageLabel.setIcon(new ImageIcon(Filename));
+    imageScrollPane.setPreferredSize(new Dimension(100, 400));
+  }
+
 }
