247f000a8a43f3e28e6cffe2a6e46afa1e2d6121
DevashishMahurkar <147368886+DevashishMahurkar@users.noreply.github.com>
2023-11-30 01:46:05 +0530

    Revert "Merging update"

--- src/grime/Main.java
+++ src/grime/Main.java
@@ -1,19 +1,14 @@
 package grime;
 
-import grime.controller.CLIController;
 import grime.controller.Controller;
 import grime.controller.ControllerFactory;
-import grime.controller.FileController;
 import grime.model.Model;
 import grime.model.RGBModel;
 import grime.utils.ArgumentParser;
 import grime.utils.Config;
 import grime.utils.ConfigurationException;
-import grime.view.CommandLineView;
 import grime.view.View;
 import grime.view.ViewFactory;
-
-import java.util.Scanner;
 
 /**
  * This class represents the main class for the program.
@@ -27,11 +22,11 @@ public class Main {
    */
   public static void main(String[] args){
     try {
-        Config config = ArgumentParser.parse(args);
-        View view = ViewFactory.createView(config);
-        Model model = new RGBModel();
-        Controller controller = ControllerFactory.createController(config, model, view);
-        controller.run();
+      Config config = ArgumentParser.parse(args);
+      View view = ViewFactory.createView(config);
+      Model model = new RGBModel();
+      Controller controller = ControllerFactory.createController(config, model, view);
+      controller.run();
     } catch (ConfigurationException e) {
       System.err.println("Failed to start the application: " + e.getMessage());
       System.exit(1);
